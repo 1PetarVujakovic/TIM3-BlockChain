3d7930a7c7dacd0aec6a2c39a6b6c3dc7e643e44
1PetarVujakovic <peravujakovic1@gmail.com>
2023-05-13 20:26:21 +0200

    Dodat SmartContract
    Dodao sam klasu SmartContract i interfejst ISmartContract. Potrebno je implementirati još klase Miner i User kako bih komplentirao klasu do kraja jer se objekti ovih klasa koriste u SmartContract-u.

---
+++ Blockchain/Common/Klase/SmartContract.cs
@@ -0,0 +1,39 @@
+﻿using Common.Interfejsi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Klase
+{
+    public class SmartContract : ISmartContract
+    {
+        public List<User> Users { get; set; }
+        public List<Miner> Miners { get; set; }
+
+        //potrebno je napraviti klase Miner i user kako ne bi bacalo greske
+        public SmartContract()
+        {
+            Users = new List<Client>();
+            Miners = new List<Miner>();
+
+        }
+
+        public bool NapraviBlock(Podatak podatak)
+        {
+
+            if (podatak.vrednost != null)
+            {
+
+                Block block = new Block(podatak);
+                Random rand = new Random();
+                int br = rand.Next(0, Miners.Count);
+                Miner miner = Miners[br];
+                throw new NotImplementedException();
+            }
+
+            return false;
+        }
+    }
+}
