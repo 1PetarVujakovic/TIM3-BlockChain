215f7734c1772c954e24e355fd5b3033f8cf69de
knezevicc <72078503+knezevicc@users.noreply.github.com>
2023-05-17 18:31:16 +0200

    Dodat SmartContractTest
    Dodat SmartContractTest.

---
+++ Blockchain/BlockchainTest/SmartContractTest.cs
@@ -0,0 +1,53 @@
+﻿using Common.Klase;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockchainTest
+{
+    [TestFixture]
+    public class SmartContractTest
+    {
+        [Test]
+        public void KreirajBlok_ReturnsTrue()
+        {
+            SmartContract sc = new SmartContract();
+            Podatak p = new Podatak();
+            p.vrednost = "test";
+
+            Miner m = new Miner();
+            sc.Miners.Add(m);
+
+            bool result = sc.NapraviBlock(p);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void KreirajBlok_ReturnFalse()
+        {
+            SmartContract sc = new SmartContract();
+            Podatak p = new Podatak();
+            p.vrednost = null;
+
+            Miner m = new Miner();
+            sc.Miners.Add(m);
+
+            bool result = sc.NapraviBlock(p);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            SmartContract sc = new SmartContract();
+
+            Assert.NotNull(sc.Clients);
+            Assert.NotNull(sc.Miners);
+        }
+    }
+}
