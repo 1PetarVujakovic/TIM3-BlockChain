4659f7f1fe47c1e577994a19438c8fb9ce91e40d
ZecevicLuka <luka.zecevic.bp@gmail.com>
2023-05-19 16:42:14 +0200

    Testovi za Miner i Client
    Urađeni testovi za Miner-a i Client-a. U klijent testu su dodate tri metode, u prve dve proveravaju Send Data, jedna vraća tačno dok druga vraća netačno. Poslednja proverava konstruktor. U majner testu ima više metoda, isto kao u drugim testovima odvojeno se pravi za return false i true. Proveravaju se ResiZadatak, Provera, ObavestiOstaleMinere, Validiraj kao i konstruktor Test.

---
+++ Blockchain/BlockchainTest/ClientTest.cs
@@ -0,0 +1,61 @@
+﻿using Common.Klase;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Common.Interfejsi;
+
+namespace BlockchainTest
+{
+    [TestFixture]
+    public class ClientTest
+    {
+
+        [Test]
+        public void SendData_ReturnsTrue()
+        {
+
+            //Arrange
+            var mock = new Mock<ISmartContract>();
+            mock.Setup(x => x.NapraviBlock(It.IsAny<Podatak>())).Returns(true);
+            Client c = new Client(mock.Object);
+            //Act
+            bool result = c.PosaljiPodatak();
+
+            //Assert
+            Assert.True(result);
+
+
+        }
+
+        [Test]
+        public void SendData_ReturnsFalse()
+        {
+
+            //Arrange
+            var mock = new Mock<ISmartContract>();
+            mock.Setup(x => x.NapraviBlock(It.IsAny<Podatak>())).Returns(false);
+            Client c = new Client(mock.Object);
+            //Act
+            bool result = c.PosaljiPodatak();
+
+            //Assert
+            Assert.False(result);
+
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+
+            SmartContract sm = new SmartContract();
+            Client c = new Client(sm);
+
+            Assert.NotNull(c.SmartContract);
+
+        }
+    }
+}
